60e71fc1c77aa6bb30238cd6532fc8843bf7e327
sswellington <wellws@pm.me>
2021-07-10 15:26:11 -0300

    feat: media, prioridade, tabuada

---
+++ python/media.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+nome = "Python"
+empresa = "IDC"
+qtde_funcionarios = 100
+mediaMensalidade = 1050.5
+total = qtde_funcionarios + mediaMensalidade
+
+print(str(total) + str(qtde_funcionarios) + nome)
---
+++ python/tabuada.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+valor=6
+
+for n in range(1,11,1):
+    print(str(valor) + " x " + str(n) + " = " + str((valor*n)))
+    

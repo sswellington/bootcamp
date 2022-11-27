51b63f167a3d50c7422ed65ac6c653abc31d5937
sswellington <sswellington@pm.me>
2022-11-27 17:28:43 -0300

    feat: DIO - estacionamento
    construir um sistema para um estacionamento,
    que será usado para gerenciar os veículos estacionados e
    realizar suas operações,
    como por exemplo adicionar um veículo,
    remover um veículo (e exibir o valor cobrado durante o período) e
    listar os veículos.

---
+++ csharp/Dio-Estacionamento/Estacionamento/Interface/IEstacionamento.cs
@@ -0,0 +1,8 @@
+namespace Dio.Estacionamento.Interface;
+
+public interface IEstacionamento
+{
+    public void AdicionarVeiculo();
+    public void RemoverVeiculo();
+    public void ListarVeiculos();
+}
---
+++ csharp/Dio-Estacionamento/Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,60 @@
+using Dio.Estacionamento.Interface;
+
+namespace Dio.Estacionamento.Models;
+
+public class Estacionamento : IEstacionamento
+{
+    private readonly decimal _precoInicial;
+    private readonly decimal _precoPorHora;
+    private List<string> _veiculos = new();
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+        _precoInicial = precoInicial;
+        _precoPorHora = precoPorHora;
+    }
+
+    public void AdicionarVeiculo()
+    {
+        Console.WriteLine("Digite a placa do veículo para estacionar:");
+        _veiculos.Add(Console.ReadLine()!);
+    }
+
+    public void RemoverVeiculo()
+    {
+        Console.WriteLine("Digite a placa do veículo para remover:");
+        string placa = Console.ReadLine()!;
+        
+        if (_veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+        {
+            Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+            int horas = 0;
+            decimal valorTotal = 0;
+
+            horas = (int)Convert.ToDecimal(Console.ReadLine());
+            valorTotal = _precoInicial + _precoPorHora * horas;
+
+            _veiculos.Remove(placa!);
+            Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+        }
+        else
+        {
+            Console.WriteLine(
+                "Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+        }
+    }
+
+    public void ListarVeiculos()
+    {
+        if (_veiculos.Any())
+        {
+            Console.WriteLine("Os veículos estacionados são:");
+            foreach (string item in _veiculos)
+                Console.WriteLine(item);
+        }
+        else
+        {
+            Console.WriteLine("Não há veículos estacionados.");
+        }
+    }
+}
